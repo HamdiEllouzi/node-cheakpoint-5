313af25333c275d2a93e5b919eb073b3b1821463
HamdiEllouzi <hlouzi089@gmail.com>
2022-01-25 20:00:38 +0100

    cheakpoints5

---
+++ models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    age: {
+        type: Number,
+
+    },
+})
+module.exports = mongoose.model('User', userSchema);
